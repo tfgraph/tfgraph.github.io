94c80b7323e3bde99be28d6db32aa55f4907be05
Sergio Garcia Prado <sergio@garciparedes.me>
2017-08-09 19:03:17 +0200

    Started to work on base webpage with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+import 'hammerjs';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MdToolbarModule, MdIconModule} from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DescriptionComponent } from './description/description.component';
+import { SocialComponent } from './social/social.component';
+import { SocialLinkComponent } from './social/social-link/social-link.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    DescriptionComponent,
+    SocialComponent,
+    SocialLinkComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdToolbarModule,
+    MdIconModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
