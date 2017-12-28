8bdf9bf18bc564c6576ed3f045663a426fa6e014
Sergio Garcia Prado <sergio@garciparedes.me>
2017-12-28 09:34:22 +0100

    Added font-awesome and angular-font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HeaderComponent} from './header/header.component';
 import {MainContainerComponent} from './main-container/main-container.component';
 import {RouterModule} from "@angular/router";
 import {Angulartics2GoogleAnalytics, Angulartics2Module} from "angulartics2";
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import {Angulartics2GoogleAnalytics, Angulartics2Module} from "angulartics2";
     MdGridListModule,
     BrowserAnimationsModule,
     Angulartics2Module.forRoot([Angulartics2GoogleAnalytics]),
+    AngularFontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
